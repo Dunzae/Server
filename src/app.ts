c8dcc218061cd1d7ce4a56af1d84a781976085b7
dunzae <dunzae99@gmail.com>
2024-11-10 18:35:51 +0900

    @Chore https로 통신할 수 있도록 설정

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import fs from "fs"
 import cors from "cors";
+import https from "https"
 import instagramRouter from "./routes/instagram/main"
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 const app = express();
 const { PORT, MONGO_URI } = process.env;
+const options = {
+  key : fs.readFileSync("./selfsigned.key"),
+  cert : fs.readFileSync("./selfsigned.crt"),
+}
 
 app.use(cors());
 app.use(express.json());
@@ -14,11 +20,12 @@ app.use(express.static("files"));
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/instagram", instagramRouter);
+const server = https.createServer(options, app);
 
 async function main() {
   try {
     await mongoose.connect(MONGO_URI as string, { serverApi: { version: "1", strict: true, deprecationErrors: true }, dbName : "portfolio" });
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
 
       console.log(
         `[Server] : Server is running at 0.0.0.0:${PORT}`
